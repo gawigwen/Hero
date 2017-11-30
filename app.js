2e3c561b5f055fea46c8bf85fd8685e7011a9255
gawigwen <19rodnce@waipahuh.k12.hi.us>
2017-11-29 21:48:16 -1000

    3

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-var age;
-var name = "Jake"
+var age = 25
+var name = "Makoto"
+var gender = "female"
+var weapon = "revolver"
 
 function warrior(age){
 	if(age < 18){
 		return "is just beginning"
-	}else if(age >= (20 >= 40)){
+	}else if(age >= (21 >= 40)){
 		return "has seen a few battles"
 	}else if(age > 40){
 		return "has lived through many epic battles"
 	}
 }
-var warriorage = warrior(45)
-console.log(warriorage)
+var warriorage = warrior(age)
+
 
 function newwarriror(name){
-	if(name == "Jake"){
-		return "Hero is our lovable hero , who has seen a few battles with his trusty whip"
+	if(name == "Makoto"){
+		return  name +" is our lovable hero , who " + warriorage + " with hes trusty " +weapon
 	}else{
 		return "You are not worthy"
 	}
@@ -23,4 +25,30 @@ function newwarriror(name){
 var character = newwarriror(name)
 console.log(character)
 
-function
+var count;
+var animal = "snake"
+function animalHouse(x, y){
+	return x + y
+}
+count = animalHouse(1,1)
+
+function animalClub(animalHouse){
+	if(count >= 2){
+		return "We have too many "+animal+"s"
+	}
+}
+console.log(animalClub(animalHouse))
+
+var hero = "Iron Man"
+function avengerAssemble(hero){
+	if(hero == "Iron Man"){
+		return "Your suit is needed"
+	}else if(hero == "Captain America"){
+		return "We need you shield"
+	}else if (hero == "Black Widow"){
+		return "We have a target for you"
+	}else{
+		return "Sorry, you are not needed fir this mission"
+	}
+}
+console.log(avengerAssemble(hero))
